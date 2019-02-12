582b89489d036097143a918a370b0ee688fa66bb
Tymofii GLUKHOV <tima@invitu.com>
2019-02-11 14:38:48 -1000

    [MIG] legal_age_notice

--- website_legal_age_notice/static/src/js/website_legal_age_notice.js
+++ website_legal_age_notice/static/src/js/website_legal_age_notice.js
@@ -1,29 +1,31 @@
-(function() {
-    'use strict';
+odoo.define('website_legal_age_notice.website_legal_age_notice', function(require) {
+    "use strict";
 
-    var website = openerp.website;
+
+    var website = require('website.website');
 
     website.ready().done(function() {
-        openerp.website.if_dom_contains('.js_wlan_message', function() {
-            var $legalAgeMessage = $('.js_wlan_message');
-            var $legalAgePopup = $('.js_wlan_message .modal');
-            $legalAgePopup.modal();
-            $('.js_wlan_btn_yes').click(function(event){
-                event.preventDefault();
-                $.ajax($(event.target).attr('href'), {
-                    'complete': function(jqXHR, textStatus){
-                        $legalAgeMessage.fadeOut('fast');
-                        $legalAgePopup.modal('hide');
-                    }
-                });
-            });
-            $('.js_wlan_btn_no').click(function(event){
-                event.preventDefault();
-                var $legalAgeQuestion = $('.js_wlan_question');
-                var $legalAgeNo = $('.js_wlan_legal_age_no');
-                $legalAgeQuestion.hide();
-                $legalAgeNo.removeClass('hidden');
+        if(!$('.js_wlan_message').length) {
+            return $.Deffered().reject("DOM doesn't contain '.js_wlan_message'");
+        }
+        var $legalAgeMessage = $('.js_wlan_message');
+        var $legalAgePopup = $('.js_wlan_message .modal');
+        $legalAgePopup.modal();
+        $('.js_wlan_btn_yes').click(function(event){
+            event.preventDefault();
+            $.ajax($(event.target).attr('href'), {
+                'complete': function(jqXHR, textStatus){
+                    $legalAgeMessage.fadeOut('fast');
+                    $legalAgePopup.modal('hide');
+                }
             });
         });
+        $('.js_wlan_btn_no').click(function(event){
+            event.preventDefault();
+            var $legalAgeQuestion = $('.js_wlan_question');
+            var $legalAgeNo = $('.js_wlan_legal_age_no');
+            $legalAgeQuestion.hide();
+            $legalAgeNo.removeClass('hidden');
+        });
     });
-})();
+});
